f298780210d736cc6f96778e6485d1ffa2c471f0
MARI MUTHU PRAKASH M - DEV <marimuthuprakash2005@gmail.com>
2026-08-30 23:55:45 +0530

    Update book_ticket.php

--- api/book_ticket.php
+++ api/book_ticket.php
@@ -1,4 +1,5 @@
-<?php >.........// api/book_ticket.php
+<?php >........
+//api/book_ticket.php
 require_once __DIR__ . '/../config/db.php';
 startAppSession();
 header('Content-Type: application/json');
